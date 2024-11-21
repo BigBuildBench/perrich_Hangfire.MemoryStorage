3cd19876cb0e624827da210bd841f0183a57f483
Linghao Zhang <starrywhuzhang@gmail.com>
2024-11-22 00:50:40 +0800

    init instance

---
+++ src/Hangfire.MemoryStorage/GlobalConfigurationExtensions.cs
@@ -0,0 +1,30 @@
+﻿using Hangfire.MemoryStorage.Database;
+
+namespace Hangfire.MemoryStorage
+{
+    public static class GlobalConfigurationExtensions
+    {
+        public static IGlobalConfiguration<MemoryStorage> UseMemoryStorage(this IGlobalConfiguration configuration)
+        {
+            var storageOptions = new MemoryStorageOptions();
+
+            return configuration.UseMemoryStorage(storageOptions);
+        }
+
+        public static IGlobalConfiguration<MemoryStorage> UseMemoryStorage(this IGlobalConfiguration configuration,
+            MemoryStorageOptions storageOptions)
+        {
+            var storage = new MemoryStorage(storageOptions);
+
+            return configuration.UseStorage(storage);
+        }
+
+        public static IGlobalConfiguration<MemoryStorage> UseMemoryStorage(this IGlobalConfiguration configuration,
+            MemoryStorageOptions storageOptions, Data data)
+        {
+            var storage = new MemoryStorage(storageOptions, data);
+
+            return configuration.UseStorage(storage);
+        }
+    }
+}
---
+++ src/Hangfire.MemoryStorage/MemoryStorage.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using Hangfire.MemoryStorage.Database;
+using Hangfire.Server;
+using Hangfire.Storage;
+
+namespace Hangfire.MemoryStorage
+{
+    public class MemoryStorage : JobStorage
+    {
+        private readonly MemoryStorageOptions _options;
+
+        private readonly object _connectionLock = new object();
+        private MemoryStorageConnection _connection;
+
+        public Data Data { get; }
+
+        public MemoryStorage() : this(new MemoryStorageOptions(), new Data())
+        {
+        }
+
+        public MemoryStorage(MemoryStorageOptions options) : this(options, new Data())
+        {
+        }
+
+        public MemoryStorage(MemoryStorageOptions options, Data data)
+        {
+            _options = options;
+            Data = data;
+        }
+
+        public override IStorageConnection GetConnection()
+        {
+            lock (_connectionLock)
+            {
+                if (_connection == null)
+                    _connection = new MemoryStorageConnection(Data, _options.FetchNextJobTimeout);
+            }
+            return _connection;
+        }
+
+        public override IMonitoringApi GetMonitoringApi()
+        {
+            return new MemoryStorageMonitoringApi(Data);
+        }
+
+#pragma warning disable 618
+        public override IEnumerable<IServerComponent> GetComponents()
+#pragma warning restore 618
+        {
+            yield return new ExpirationManager(Data, _options.JobExpirationCheckInterval);
+            yield return new CountersAggregator(Data, _options.CountersAggregateInterval);
+        }
+    }
+}
